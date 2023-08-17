2a47158dba8bf3f8e8a20f9ebeee77beb431db74
Batyrhan Zhanabayev <bzhanabaev@gmail.com>
2023-08-18 00:31:05 +0600

    услуги, форма выбора, и т.д.

--- server/server.js
+++ server/server.js
@@ -6,35 +6,91 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+const uslugiList = [
+  { name: "Кузов", prices: { "Легковая": 2500, "Кроссовер": 3000, "Внедорожник": 3500 } },
+  { name: "Салон", prices: { "Легковая": 2000, "Кроссовер": 2500, "Внедорожник": 3000 } },
+  { name: "Кузов Салон", prices: { "Легковая": 3500, "Кроссовер": 4000, "Внедорожник": 5000 } },
+  { name: "Багажник", prices: { "Легковая": 500, "Кроссовер": 700, "Внедорожник": 1000 } },
+  { name: "Нанесение воска", prices: { "Легковая": 700, "Кроссовер": 1000, "Внедорожник": 1000 } },
+  { name: "Детейлинг химчистка", prices: { "Легковая": 50000, "Кроссовер": 55000, "Внедорожник": 70000 } },
+  { name: "Отбивка радиатора", prices: { "Легковая": 700, "Кроссовер": 700, "Внедорожник": 1000 } },
+  { name: "Сухой туман", prices: { "Легковая": 2000, "Кроссовер": 2500, "Внедорожник": 3000 } },
+  { name: "Балон", prices: { "Легковая": 600, "Кроссовер": 700, "Внедорожник": 700 } },
+  { name: "Балон с диском", prices: { "Легковая": 1000, "Кроссовер": 1000, "Внедорожник": 1000 } },
+  { name: "Комплексная мойка", prices: { "Легковая": 4800, "Кроссовер": 5500, "Внедорожник": 6500 } },
+  { name: "Чернение шин", prices: { "Легковая": 500, "Кроссовер": 500, "Внедорожник": 500 } },
+  { name: "Отбивка кузова без пены", prices: { "Легковая": 1200, "Кроссовер": 1500, "Внедорожник": 2000 } },
+  { name: "Отбивка с пеной", prices: { "Легковая": 1500, "Кроссовер": 1800, "Внедорожник": 2500 } },
+  { name: "Двигатель", prices: { "Легковая": 3000, "Кроссовер": 4000, "Внедорожник": 6000 } },
+  { name: "Полировка салона", prices: { "Легковая": 1000, "Кроссовер": 1500, "Внедорожник": 2000 } },
+  { name: "Химчистка салона", prices: { "Легковая": 25000, "Кроссовер": 35000, "Внедорожник": 45000 } },
+  { name: "Полики", prices: { "Легковая": 500, "Кроссовер": 500, "Внедорожник": 500 } }
+];
 
 mongoose.connect('mongodb://127.0.0.1:27017/Moika', { useNewUrlParser: true, useUnifiedTopology: true });
-
 
 const ZakazSchema = new mongoose.Schema({
   carBrand: String,
   carNumber: String,
-  carType: String
+  carType: String,
+  uslugi: [String] 
 });
+
 
 const Zakaz = mongoose.model('Zakaz', ZakazSchema, 'Zakaz');
 
+const UslugaSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  prices: {
+    "Легковая": Number,
+    "Кроссовер": Number,
+    "Внедорожник": Number
+  }
+});
+
+const Usluga = mongoose.model('Usluga', UslugaSchema);
+async function initializeUslugi() {
+  for (let uslugaData of uslugiList) {
+    const existingUsluga = await Usluga.findOne({ name: uslugaData.name });
+    if (existingUsluga) {
+      existingUsluga.prices = uslugaData.prices;
+      await existingUsluga.save();
+    } else {
+      const newUsluga = new Usluga(uslugaData);
+      await newUsluga.save();
+    }
+  }
+  console.log("Uslugi initialized/updated in the database.");
+}
+
+// Вызовите эту функцию при запуске сервера (можно раскомментировать следующую строку):
+ initializeUslugi();
 
 app.post('/add', async (req, res) => {
-  console.log("Получен запрос:", req.body); // Логирование полученных данных
+  console.log("Получен запрос:", req.body);
+  const newZakaz = new Zakaz(req.body); // Создаем новый заказ при каждом запросе
 
-  const newZakaz = new Zakaz(req.body);
-  
   try {
     const savedZakaz = await newZakaz.save();
-    console.log("Запись успешно сохранена:", savedZakaz); // Логирование сохраненной записи
+    console.log("Запись успешно сохранена:", savedZakaz);
     res.status(200).send(savedZakaz);
   } catch (err) {
-    console.error("Ошибка при сохранении:", err); // Логирование ошибки
+    console.error("Ошибка при сохранении:", err);
     res.status(500).send(err);
   }
 });
 
-
+app.get('/uslugi', async (req, res) => {
+  try {
+    const uslugi = await Usluga.find();
+    res.status(200).json(uslugi);
+  } catch (err) {
+    res.status(500).json({ message: "Ошибка при получении списка услуг", error: err });
+  }
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,15 @@
           <option value="Внедорожник">Внедорожник</option>
         </select>
       </div>
+      <Multiselect
+        v-model="selectedUslugi"
+        :options="uslugi"
+        :multiple="true"
+        track-by="name"
+        label="name"
+        placeholder="Выберите услуги"
+      ></Multiselect>
+
       <button type="submit">Добавить</button>
     </form>
   </div>
@@ -28,12 +37,21 @@
 
 <script>
 import axios from "axios";
+import Multiselect from "vue-multiselect";
+import "vue-multiselect/dist/vue-multiselect.min.css";
+
 export default {
+  components: {
+    Multiselect,
+  },
   data() {
     return {
       carBrand: "",
       carNumber: "",
       carType: "",
+      uslugi: [],
+      selectedUslugi: [],
+      selectedPrice: 0, // Массив для хранения выбранных услуг
     };
   },
   methods: {
@@ -43,6 +61,7 @@ export default {
           carBrand: this.carBrand,
           carNumber: this.carNumber,
           carType: this.carType,
+          uslugi: this.selectedUslugi,
         })
         .then((response) => {
           console.log("Данные успешно добавлены:", response.data);
@@ -52,5 +71,74 @@ export default {
         });
     },
   },
+  async mounted() {
+    try {
+      const response = await axios.get("http://localhost:3001/uslugi");
+      this.uslugi = response.data;
+    } catch (err) {
+      console.error("Ошибка при получении списка услуг:", err);
+    }
+  },
 };
 </script>
+<style scoped>
+div {
+  font-family: "Arial", sans-serif;
+  max-width: 500px;
+  margin: 50px auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+div > div {
+  margin-bottom: 15px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: bold;
+}
+
+input,
+select {
+  width: 100%;
+  padding: 8px 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+}
+
+button {
+  display: block;
+  width: 100%;
+  padding: 10px 15px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+div > div.uslugi {
+  display: grid;
+  grid-template-columns: repeat(
+    2,
+    1fr
+  ); /* Разделите список услуг на две колонки */
+  gap: 10px; /* Отступ между элементами */
+}
+</style>
